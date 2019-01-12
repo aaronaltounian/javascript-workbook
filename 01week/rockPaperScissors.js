1bc81dac970da0b55af54000ef13d6ef4179b0c3
Aaron Altounian <aaron.altounian@gmail.com>
2019-01-12 01:49:34 -0600

    built out rockPaperScissors function with validity check and win state checks!

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -8,10 +8,73 @@ const rl = readline.createInterface({
 });
 
 
-function rockPaperScissors(hand1, hand2) {
+function rockPaperScissors(a, b) {
+  
+  // define variables for win states:
+  const tie = "It's a tie!";
+  const player1Wins = "Player 1 Wins!";
+  const player2Wins = "Player 2 Wins!";
+  // take the function's arguments (player choices) and make them lowercase so they still run even if a player capitalizes a letter:
+  let hand1 = a.toLowerCase();
+  let hand2 = b.toLowerCase();
 
-  // Write code here
+  // check if the choices are valid/create a boolean so it can be compared in the results stage:
+  function choiceChecker(hand1, hand2) {
+    switch(hand1){
+      // if player 1 picks a valid turn then break and move on to checking player 2, or else return that it's a false move:
+      case 'rock':
+      case 'paper':
+      case 'scissors':
+        break;
+      default: 
+        return false;
+    }
+    // check player 2's choice and if both player's choices are valid then return true, if player 2's choice is not valid then return false:
+    switch(hand2){
+      case 'rock':
+      case 'paper':
+      case 'scissors':
+        return true;
+      default: 
+        return false;
+    }
+  }
+  // define a boolean variable created from the validity checking function:
+  let isValid = choiceChecker(hand1, hand2);
 
+  //     !!!   Win States   !!!
+  // if the choices aren't valid, say so:
+  if (!isValid) {
+    return 'Try again, choose either rock, paper, or scissors!';
+  }
+  // if the choices are the same, the game results in a tie:
+  else if (hand1 === hand2) {
+    return tie;
+  } 
+  // or else if player one picks rock, determine winner based off player 2's choice:
+  else if (hand1 === 'rock') {
+    if (hand2 === 'paper') {
+      return player2Wins;
+    } else {
+      return player1Wins;
+    }
+  }
+  // or else if player 1 picks paper, determine winner based off player 2's choice:
+  else if (hand1 === 'paper') {
+    if(hand2 === 'rock') {
+      return player1Wins;
+    } else {
+      return player2Wins;
+    }
+  }
+  // finally, if player 1 picks scissors, determine winner based off player 2's choice:
+  else if (hand1 === 'scissors') {
+    if (hand2 === 'rock') {
+      return player2Wins;
+    } else {
+      return player1Wins;
+    }
+  }
 }
 
 function getPrompt() {
