830c7a6434e86ca4ef99519350378d9e23ac0684
Aaron Altounian <aaron.altounian@gmail.com>
2019-01-17 20:33:40 -0600

    wrote new tests

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert').strict;
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -92,19 +92,34 @@ if (typeof describe === 'function') {
 
   describe('#rockPaperScissors()', () => {
     it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+      assert.strictEqual(rockPaperScissors('rock', 'rock'), "It's a tie!");
+      assert.strictEqual(rockPaperScissors('paper', 'paper'), "It's a tie!");
+      assert.strictEqual(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
     });
     it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+      assert.strictEqual(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.strictEqual(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.strictEqual(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
     });
     it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+      assert.strictEqual(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
+      assert.strictEqual(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
+      assert.strictEqual(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+    it('should detect all scenarios in which hand one wins', () => {
+      assert.strictEqual(rockPaperScissors('rock', 'scissors'), 'Hand one wins!');
+      assert.strictEqual(rockPaperScissors('paper', 'rock'), 'Hand one wins!');
+      assert.strictEqual(rockPaperScissors('scissors', 'paper'), 'Hand one wins!');
+    });
+    it('should detect all scenarios in which hand two wins', () => {
+      assert.strictEqual(rockPaperScissors('rock', 'paper'), 'Hand two wins!');
+      assert.strictEqual(rockPaperScissors('paper', 'scissors'), 'Hand two wins!');
+      assert.strictEqual(rockPaperScissors('scissors', 'rock'), 'Hand two wins!');
+    });
+    it('should only accept valid inputs', () => {
+      assert.strictEqual(rockPaperScissors('wreck', 'paper'), 'Try again, choose either rock, paper, or scissors!');
+      assert.strictEqual(rockPaperScissors('rock', 'proper'), 'Try again, choose either rock, paper, or scissors!');
+      assert.strictEqual(rockPaperScissors('nothing', 'wrong'), 'Try again, choose either rock, paper, or scissors!');
     });
   });
 } else {
