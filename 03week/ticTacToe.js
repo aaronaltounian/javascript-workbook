d88e376cd31d2ba34a84378a458e90a7febdbb7f
Aaron Altounian <aaron.altounian@gmail.com>
2019-01-24 03:51:00 -0600

    updated to arrow functions, changed horiz win check method. few minor other things

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -6,6 +6,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
@@ -14,7 +15,7 @@ let board = [
 
 let playerTurn = 'X';
 
-function printBoard() {
+let printBoard = () => {
   console.log('   0  1  2');
   console.log('0 ' + board[0].join(' | '));
   console.log('  ---------');
@@ -34,31 +35,33 @@ let reset = () => {
   turnCount = 1;
 }
 
-function horizontalWin() {
-  // make a string depending on the playerTurn, 'XXX' or 'OOO' to compare against:
-  let playerString = playerTurn + playerTurn + playerTurn;
-  // method using for/in with string comparators and joining the row arrays together:
-  for( let i in board ) {
-    // join each subarray into a string with no spaces:
-    let rowString = board[i].join('');
-    // compare rowString against playerString to detect horizontal win:
-    if(rowString == playerString) {
-      return true;
-    }
-    else return false;
+// make a string depending on the playerTurn, e.g. 'XXX' or 'OOO' to compare against:
+let playerString = playerTurn + playerTurn + playerTurn;
+
+let horizontalWin = () => {
+  // method for row win state using .every():
+  if(
+    (board[0].every(x => x == playerTurn)) ||
+    (board[1].every(x => x == playerTurn)) ||
+    (board[2].every(x => x == playerTurn))
+  ) {
+    return true;
   }
-  // alternative method for row win state using .every():
-  // if(
-  //   (board[0].every(x => x == playerTurn)) ||
-  //   (board[1].every(x => x == playerTurn)) ||
-  //   (board[2].every(x => x == playerTurn))
-  // ) {
-  //   return true;
+  else return false;
+
+  // method using for/in with string comparators and joining the row arrays together (not working as intended):
+  // for( let i in board ) {
+  //   // join each subarray into a string with no spaces:
+  //   let rowString = board[i].join('');
+  //   // compare rowString against playerString to detect horizontal win:
+  //   if( rowString == playerString ) {
+  //     return true;
+  //   }
+  //   else return false;
   // }
-  // else return false;
 }
 
-function verticalWin() {
+let verticalWin = () => {
   // brute force method that i'm not super happy with, checking each possible win iteration using && and || in an if statement:
   if(
     (board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn) ||
@@ -84,7 +87,7 @@ let diagonalWin = () => {
 }
 
 // make checkForWin return true if any of the win state checks are true:
-function checkForWin() {
+let checkForWin = () => {
   if( horizontalWin() || verticalWin() || diagonalWin() ) {
     return true;
   }
@@ -92,7 +95,7 @@ function checkForWin() {
 }
 
 // create a function to return true if the choices are valid (note: added number cases cuz prewritten test wouldn't work with strings):
-let choiceChecker = (row, column) => {
+let choiceChecker = ( row, column ) => {
   switch(row){
     case '0':
     case '1':
@@ -118,33 +121,35 @@ let choiceChecker = (row, column) => {
 }
 
 // create function to flip turn (just to make it easier to read in the ticTacToe function):
-let flipTurn = () => { playerTurn == 'X' ? playerTurn = 'O' : playerTurn = 'X'; }
+let flipTurn = () => {
+  playerTurn == 'X' ? playerTurn = 'O' : playerTurn = 'X';
+}
 
 // create turn counter variable as storage to help determine a tie/cat's game:
 let turnCount = 1;
 
-function ticTacToe(row, column) {
+let ticTacToe = ( row, column ) => {
   // Check if turn is valid:
   let isValid = choiceChecker(row, column);
   // If turn is not valid, say so:
-  if(!isValid) {
+  if( !isValid ) {
     console.log('Try again, enter a valid row and column!');
   }
   // else if chosen space is already occupied, say so:
-  else if(board[row][column] != ' ') {
+  else if( board[row][column] != ' ' ) {
     console.log('That space is already taken, try again!');
   }
   // or else put the current turn ('X' or 'O') in the array space represented by the inputs:
   else {
     board[row][column] = playerTurn;
     // check if there's a win, if so log it and reset the board:
-    if(checkForWin()){
-      console.log(`${playerTurn} wins!`);
+    if( checkForWin() ) {
+      console.log( `${playerTurn} wins!` );
       reset();
     }
     // or else if there's no winner and all the spaces are full, log tie game and reset:
     else if( !checkForWin() && turnCount == 9 ) {
-      console.log("Cat's game! Tie!");
+      console.log( "Cat's game! Tie!" );
       reset();
     }
     // or else flip the turn and add 1 to the turn counter:
@@ -155,9 +160,9 @@ function ticTacToe(row, column) {
   }
 }
 
-function getPrompt() {
+let getPrompt = () => {
   printBoard();
-  console.log("It's Player " + playerTurn + "'s turn.");
+  console.log( `It's player ${playerTurn}'s turn.` );
   rl.question('row: ', (row) => {
     rl.question('column: ', (column) => {
       ticTacToe(row, column);
